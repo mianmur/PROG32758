19bcd3256f654448ac32bc59e250f39ac250c7f4
mianmur <mianmur@sheridancollege.ca>
2022-10-04 18:02:55 -0400

    change int to date

--- assignments/exercise4/src/main/java/ca/sheridancollege/exercise4/beans/Movie.java
+++ assignments/exercise4/src/main/java/ca/sheridancollege/exercise4/beans/Movie.java
@@ -3,6 +3,9 @@ package ca.sheridancollege.exercise4.beans;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,5 +13,7 @@ import lombok.NoArgsConstructor;
 public class Movie {
     private String name;
     private String director;
-    private int yrRelease;
+
+    @DateTimeFormat(pattern = "yyyy")
+    private Date yrRelease;
 }
